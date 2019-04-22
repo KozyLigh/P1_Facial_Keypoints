2ab17622149a255345e119912985e1c3affaa0fb
Marko Kozjak <kozypato@gmail.com>
2019-04-22 23:01:16 +0200

    CV project first commit

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,18 +19,49 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 5) 
+        self.conv1_bn = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(2, 2) 
+
+        self.conv2 = nn.Conv2d(32, 64, 5) 
+        self.conv2_bn = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(2, 2)  
+        self.conv2_drop = nn.Dropout(p=0.25) 
+        
+        self.conv3 = nn.Conv2d(64, 128, 5)  
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(2, 2)     
+        
+        self.fc1 = nn.Linear(128*24*24, 256) 
+        self.fc1_bn = nn.BatchNorm1d(256)        
+        self.fc1_drop = nn.Dropout(p=0.35)
+        self.fc2 = nn.Linear(256, 68*2)  
+        
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
-
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        # Convolutional and pooling layers
+        x = self.pool1(F.leaky_relu(self.conv1(x)))
+        x = self.conv1_bn(x)
+        x = self.pool2(F.leaky_relu(self.conv2(x)))
+        x = self.conv2_bn(x)
+        x = self.conv2_drop(x)
+        x = self.pool3(F.leaky_relu(self.conv3(x)))
+        x = self.conv3_bn(x)
+        
+        # Fully-Connected layers
+        x = x.view(x.size(0), -1)
+        x = F.leaky_relu(self.fc1(x))
+        x = self.fc1_bn(x)
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
